582933748e320742a028e6e7edd6173132814825
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-27 13:53:34 -0300

    Implementing report generator class

--- src/main/java/br/com/erudio/services/implementations/ReportServiceImpl.java
+++ src/main/java/br/com/erudio/services/implementations/ReportServiceImpl.java
@@ -3,14 +3,24 @@ package br.com.erudio.services.implementations;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.services.ReportService;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -23,21 +33,22 @@ public class ReportServiceImpl implements ReportService {
     	FileUtils fileUtils = new FileUtils();
 	
 //    	DataBeanMaker dataBeanMaker = new DataBeanMaker();
-//		InputStream inputStream = getClass().getResourceAsStream("template/test_jasper.jrxml");
-//        
+		InputStream inputStream = getClass().getResourceAsStream("template/test_jasper.jrxml");
+        
 //        ArrayList<DataBean> dataBeanList = dataBeanMaker.getDataBeanList();
 //        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataBeanList);
-//		Map parameters = new HashMap();
-//		File pdf = File.createTempFile("output.", ".pdf");
-//        try {
-//			JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
-//			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters, beanColDataSource);
-//			JasperExportManager.exportReportToPdfStream(jasperPrint, new FileOutputStream(pdf));
-//		} catch (Exception e) {
-//			logger.error(e);
-//		}
-//        byte[] array = getBytesFromFile(pdf);
-		return null;
+        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(null);
+		Map parameters = new HashMap();
+		File pdf = File.createTempFile("output.", ".pdf");
+        try {
+			JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters, beanColDataSource);
+			JasperExportManager.exportReportToPdfStream(jasperPrint, new FileOutputStream(pdf));
+		} catch (Exception e) {
+			logger.error(e);
+		}
+        byte[] array = getBytesFromFile(pdf);
+		return array;
 	}
 	
 	public byte[] getBytesFromFile(File file) {
