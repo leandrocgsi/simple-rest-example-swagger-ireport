7e1f05d8fe40f3d80b5aadedf5d5d13ec19081ab
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-27 22:29:59 -0300

    Adding report endpoint to swagger

--- src/main/java/br/com/erudio/Application.java
+++ src/main/java/br/com/erudio/Application.java
@@ -29,7 +29,7 @@ public class Application {
     public SwaggerSpringMvcPlugin groupOnePlugin() {
        return new SwaggerSpringMvcPlugin(swaggerConfig)
            .apiInfo(apiInfo())
-           .includePatterns("/person.*?", "/greeting.*?")
+           .includePatterns("/person.*?", "/greeting.*?", "/report.*?")
            .swaggerGroup("admin");
     }
     
